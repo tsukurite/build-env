ce598c41f3a085c7a62787f9b0c981da3c64460f
sasaplus1 <sasaplus1@gmail.com>
2015-01-29 22:15:51 +0900

    changed to install compass at setup

--- setup.bat
+++ setup.bat
@@ -6,4 +6,7 @@ set NODE_PATH="%~dp0\bin\node_modules\npm\node_modules;%~dp0\bin\node_modules\np
 call npm.cmd install -g npm
 call npm.cmd install
 
+call gem.bat update --no-document --system
+call gem.bat install compass --no-document --version 1.0.3
+
 rem vim:ff=dos:
